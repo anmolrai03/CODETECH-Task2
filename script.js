d18a966b8ab2c85244f3dd3aded0f80e113a7771
Anmol Rai <purevoidexists@gmail.com>
2024-07-23 08:22:43 +0530

    later stage

--- script.js
+++ script.js
@@ -11,12 +11,24 @@ let uncompletedTask = 0;
 let taskHTML = ``;
 
 function saveToStorage() {
-    localStorage.setItem('data' , taskSection);
+    localStorage.setItem('data' , uncompleteTaskContainerEle.innerHTML);
+}
+
+function saveNumber(){
+    localStorage.setItem('number' , numberOfTaskEle.innerHTML);
+}
+
+function getNumber(){
+    numberOfTaskEle.innerHTML = localStorage.getItem("number");
 }
 
 function getFromStorage() {
-    console.log('data')
-    localStorage.getItem('data');
+    // console.log('data')
+    // localStorage.getItem('data');
+    // console.log(JSON.parse(localStorage.getItem('data')))
+    uncompleteTaskContainerEle.innerHTML = localStorage.getItem('data');
+    getNumber();
+
 }
 
 function updateRemainingTaskNumber(){
@@ -25,7 +37,7 @@ function updateRemainingTaskNumber(){
     } else {
         numberOfTaskEle.innerText = 0;
     }
-    saveToStorage();
+    saveNumber();
 }
 
 function addTask(taskMsg){
@@ -38,7 +50,7 @@ function addTask(taskMsg){
             </span>
             
             <span class="options">
-                <img src="https://cdn-icons-png.flaticon.com/512/1160/1160515.png" alt="edit" class="edit-icon">
+                
 
                 <img src="https://cdn-icons-png.flaticon.com/512/1828/1828843.png" alt="delete" class="delete-icon">
             </span>
@@ -56,21 +68,19 @@ function deleteTask(deleteTaskEle) {
     liToDelete.remove();
     uncompletedTask--;
     updateRemainingTaskNumber();
-    saveToStorage();
 }
 
-function editTask(taskMsgEle) {
-    const taskMsg = taskMsgEle.innerText;
-    // const liContainer = taskMsgEle.parentNode.parentNode;
-    inputEle.value = taskMsg;
-    btnEle.innerText = 'Edit';
-    btnEle.addEventListener('click' , (e) => {
-        let newMsg = inputEle.value;
-        console.log(`new value is: '${newMsg}'`);
-        // taskMsg.innerText = newMsg;
-    })
-    saveToStorage();
-}
+// function editTask(taskMsgEle) {
+//     const taskMsg = taskMsgEle.innerText;
+//     // const liContainer = taskMsgEle.parentNode.parentNode;
+//     inputEle.value = taskMsg;
+//     btnEle.innerText = 'Edit';
+//     btnEle.addEventListener('click' , (e) => {
+//         let newMsg = inputEle.value;
+//         console.log(`new value is: '${newMsg}'`);
+//     }, false)
+//     // saveToStorage();
+// }
 
 function checkMarkToggle(checkmarkImage, taskMsgEle) {
     
@@ -88,45 +98,45 @@ function checkMarkToggle(checkmarkImage, taskMsgEle) {
     }
     checkmarkImage.classList.toggle('unchecked');
     // console.log(checkmark.className)
-    saveToStorage();
 }
 
-function clickHandle(){
-    btnEle.addEventListener('click', (e) => {
-        e.preventDefault();
-        let taskmsg = inputEle.value;
-        if(taskmsg === '' || taskmsg === ' '){
-            errorEle.style.display = 'block';
-            errorEle.classList.add('shake-animation');
-            setTimeout( () => {
-                errorEle.classList.remove('shake-animation');
-            }, 600);
-        } else{
-            addTask(taskmsg);
-            errorEle.style.display = 'none';
-        }
-        inputEle.value = '';
-    })
-}
+// function clickHandle(){
+//     btnEle.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         let taskmsg = inputEle.value;
+//         if(taskmsg === '' || taskmsg === ' '){
+//             errorEle.style.display = 'block';
+//             errorEle.classList.add('shake-animation');
+//             setTimeout( () => {
+//                 errorEle.classList.remove('shake-animation');
+//             }, 600);
+//         } else{
+//             addTask(taskmsg);
+//             errorEle.style.display = 'none';
+//         }
+//         inputEle.value = '';
+//     })
+// }
 
-clickHandle();
+// clickHandle();
 
-// btnEle.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     let taskmsg = inputEle.value;
+btnEle.addEventListener('click', (e) => {
+    e.preventDefault();
+    let taskmsg = inputEle.value;
 
-//     if(taskmsg === '' || taskmsg === ' '){
-//         errorEle.style.display = 'block';
-//         errorEle.classList.add('shake-animation');
-//         setTimeout( () => {
-//             errorEle.classList.remove('shake-animation');
-//         }, 600);
-//     } else{
-//         addTask(taskmsg);
-//         errorEle.style.display = 'none';
-//     }
-//     inputEle.value = '';
-// })
+    if(taskmsg === '' || taskmsg === ' '){
+        errorEle.style.display = 'block';
+        errorEle.classList.add('shake-animation');
+        setTimeout( () => {
+            errorEle.classList.remove('shake-animation');
+        }, 600);
+    } else{
+        // console.log('btn task msg:',taskmsg);
+        addTask(taskmsg);
+        errorEle.style.display = 'none';
+    }
+    inputEle.value = '';
+} , false);
 
 uncompleteTaskContainerEle.addEventListener('click' , (e) => {
 
@@ -157,10 +167,11 @@ uncompleteTaskContainerEle.addEventListener('click' , (e) => {
         deleteTask(element);
     }
 
-});
+}, false);
 
 
-getFromStorage();
+// getFromStorage();
+
 
 // function clickHandleTaskArea() {
 //     const taskDataContainer = document.querySelectorAll('.task-data');
